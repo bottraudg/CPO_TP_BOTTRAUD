64d63b8fa25bd5a8589f03d47a33413d81ce1ef4
guilenebottraud <guilenebottraud@localhost>
2022-10-25 17:02:19 +0200

    Création d'un nouveau projet versionné : TP2 bieres

---
+++ TP2_Bieres_BOTTRAUD/src/tp2_bieres_bottraud/TP2_Bieres_BOTTRAUD.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_bottraud;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+ 
+public class TP2_Bieres_BOTTRAUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    BouteilleBiere uneBiere = new BouteilleBiere() ;
+    uneBiere.nom = "Cuvée des trolls"; 
+    uneBiere.degreAlcool = 7.0 ;
+    uneBiere.brasserie = " Dubuisson ";
+    uneBiere.ouverte = false;
+    
+            uneBiere.lireEtiquette();
+    }
+    
+}
