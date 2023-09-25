a4f8b53baf4c1ada1afcbfff98f595ea0bfac851
guilenebottraud <guilenebottraud@localhost>
2023-09-25 14:53:02 +0200

    fin exercice 3 partie 3

---
+++ TP0_BOTTRAUD/TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+    System.out.println("1) add");
+        
+   
+    System.out.println("2) substarct");
+    
+    
+    System.out.println("3) multiply");
+    
+    
+    System.out.println("4) divide");
+    
+    
+    System.out.println("5) modulo");
+    
+    Scanner sc = new Scanner(System.in);
+        int operateur = sc.nextInt();
+
+
+            
+    }
+    
+}
