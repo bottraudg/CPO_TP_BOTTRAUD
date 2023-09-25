0d7bf586992202da3146a0c6ae2ea9363cebd1bc
guilenebottraud <guilenebottraud@localhost>
2023-09-25 15:27:21 +0200

    exercice 6 TP0

---
+++ TP0_BOTTRAUD/TP0/src/tp0/TP0.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class TP0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in); 
+System.out.println("Bonjour, quel est votre prenom ?"); 
+prenom = sc.nextLine();
+    }
+    
+}
