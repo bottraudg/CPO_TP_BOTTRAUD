485fc81e499dcaba4b58ec98585e5c46c4a1a3f1
guilenebottraud <guilenebottraud@localhost>
2023-10-11 18:31:44 +0200

    TP2 exercice 1 bieres fini

---
+++ TP2_Bieres_BOTTRAUD/src/tp2_bieres_bottraud/TP2_Bieres_BOTTRAUD.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_bottraud;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class TP2_Bieres_BOTTRAUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", (float) 7.0,"Dubuisson") ;
+      uneBiere.lireEtiquette();
+      
+       BouteilleBiere secondeBiere = new BouteilleBiere("Leffe", (float) 6.6,"Abbaye de Leffe") ;
+      secondeBiere.lireEtiquette();
+      
+  
+   
+    
+    
+    
+    BouteilleBiere B3 = new BouteilleBiere("Delirium Tremens", (float) 8.5,"Brasserie Huyghe") ;
+        B3.Décapsuler(); //fonction  decapsuler 
+        BouteilleBiere B2 = new BouteilleBiere("Rince cochon", (float) 8.5,"Brasserie Rince Cochon") ;
+        BouteilleBiere B1 = new BouteilleBiere("BMB Blanche", (float) 5.6,"Brasserie Mont Blanc") ;
+        B1.Décapsuler();
+        
+        System.out.println(B1 + "\n" + B2 + "\n" + B3); //permet de lire avec toString()
+    }// fin du main 
+    
+}
