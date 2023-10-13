5b7557c906d9f5d5f50324372a581dcb46e11e5b
guilenebottraud <guilenebottraud@localhost>
2023-10-13 14:37:19 +0200

    TP2 exercice 4

---
+++  TP2_relation_1_BOTTRAUD/src/tp2_relation_1_bottraud/Personne.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_bottraud;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class Personne {
+// création des variables 
+
+    String prenom;
+    String nom;
+    
+    Voiture[] liste_voitures;
+    int nbVoitures;
+     
+
+    public Personne(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
+        liste_voitures = new Voiture[3]; // création d'un tableau 3 cases (car 3 voitures max)
+
+    }
+ public boolean ajouter_voiture(Voiture voiture_a_ajouter) {
+     boolean x = true;
+     if (nbVoitures == 3){
+         x = false;
+     }
+     if (voiture_a_ajouter.Proprietaire != null){
+         x = false;
+     }
+     liste_voitures[nbVoitures] = voiture_a_ajouter;
+     nbVoitures ++;
+     voiture_a_ajouter.Proprietaire = this ;
+     return x;
+    }
+
+}
