849d10cd7551e0fb59aec3e8f192164d4e893238
guilenebottraud <guilenebottraud@localhost>
2023-10-13 10:24:14 +0200

    TP2 exercice 2 convertisseur fini

---
+++ TP2_convertisseurObjet_BOTTRAUD/src/tp2_convertisseurobjet_bottraud/Convertisseur.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_bottraud;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class Convertisseur {
+int nbConversions;
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+        // CONVERTION CELCIUS VERS KELVIN
+    public static double CelciusVersKelvin(double tCelcius) {
+        double tKelvin;
+        tKelvin = tCelcius + 273.15;
+        return tKelvin;
+    }
+    // CONVERTION KELVIN VERS CELCIUS
+
+    public static double KelvinVersCelcius(double tKelvin) {
+        double tCelcius;
+        tCelcius = tKelvin - 273.15;
+        return tCelcius;
+    }
+    // CONVERTION FARENHEIT VERS CELCIUS
+
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        double tCelcius;
+        tCelcius = tFarenheit - 32;
+        return tCelcius;
+    }
+    // CONVERTION CELCIUS VERS FARENHEIT
+
+    public static double CelciusVersFarenheit(double tCelcius) {
+        double tFarenheit;
+        tFarenheit = tCelcius - 32;
+        return tFarenheit;
+    }
+
+    // CONVERTION KELVIN VERS FARENHEIT
+    public static double KelvinVersFarenheit(double tKelvin) {
+        double tFarenheit;
+        tFarenheit = tKelvin - 460;
+        return tFarenheit;
+    }
+
+    // CONVERTION FARENHEIT VERS KELVIN
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        double tKelvin;
+        tKelvin = tFarenheit + 460;
+        return tKelvin;
+
+    }
+   @Override
+public String toString () {
+  return "nb de conversions"+ nbConversions;
+    }
+}
