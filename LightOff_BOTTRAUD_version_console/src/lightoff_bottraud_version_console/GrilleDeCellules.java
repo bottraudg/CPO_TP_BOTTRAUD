1f722fa09c291f9beb96330872e7f8ae10404e69
guilenebottraud <guilenebottraud@localhost>
2023-10-27 10:01:17 +0200

    la CelluleLumineuse a été ajoutée et est fonctionnelle

---
+++ LightOff_BOTTRAUD_version_console/src/lightoff_bottraud_version_console/GrilleDeCellules.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_bottraud_version_console;
+
+/**
+ *
+ * @author guilenebottraud
+ */
+public class GrilleDeCellules {
+   int nbLignes=0;
+   int nbColonnes=0;
+    CelluleLumineuse[][] matriceCellules = new  CelluleLumineuse[7][7];
+   
+   
+   
+
+    public GrilleDeCellules(int nbLignes, int nbColonnes) {
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
